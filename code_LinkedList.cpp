1b95fc4abcdc94623cad0a0bce9aa871e7e69ee5
Zyrko <91777809+fgyuawegi@users.noreply.github.com>
2024-03-19 19:35:59 +0800

    Update code_LinkedList.cpp

--- code_LinkedList.cpp
+++ code_LinkedList.cpp
@@ -1,51 +1,94 @@
 #include <iostream>
 using namespace std;
-struct Node {
-    int val;
-    Node* next;
+struct node {
+    int index;
+    node* next;
 };
-Node* readData(int N) {
-    Node* head;
+node* readFunc(int M) {
+    node* head=NULL;
+    node* tail=NULL;
     bool flag = 0;
-    while (N--) {
+    while (M--) {
         int n;
         cin >> n;
         if (flag == 0) {
-            Node* A = new Node;
+            node* A = new node;
             A->next = NULL;
-            A->val = n;
+            A->index = n;
             head = A;
+            tail = A;
             flag = 1;
             continue;
         }
-        Node* A = new Node;
-        A->val = n;
-        A->next = head;
-        head = A;
+        node* A = new node;
+        A->index = n;
+        A->next = NULL;
+        tail->next = A;
+        tail = A;
     }
     return head;
 }
+void insertFunc(node** head,int idx,int n)
+{
+    node* cur = *head, * prev = NULL;
+            for (int i = 0; i < idx; i++) {
+                prev = cur;
+                cur = cur->next;
+            }
+            if (prev == NULL) {
+                node* A = new node;
+                A->index = n;
+                A->next = cur;
+                *head = A;
+            }
+            else {
+                node* A = new node;
+                A->index = n;
+                prev->next = A;
+                A->next = cur;
+            }
+}
+void deletleFunc(node** head,int idx)
+{
+    node* cur = *head, * prev = NULL;
+            for (int i = 0; i < idx; i++) {
+                prev = cur;
+                cur = cur->next;
+            }
+            if (prev == NULL) {
+                *head = cur->next;
+                delete cur;
+            }
+            else {
+                prev->next = cur->next;
+            }
+}
 int main()
 {
-    int N;
-    cin >> N;
-    Node* head = readData(N);
-    bool f = 0;
-    int now;
-    for (Node* cur = head; cur != NULL; cur = cur->next) {
+    int M, N;
+    cin >> M >> N;
+    node* head = readFunc(M);
+    for (int i = 0; i < N; i++) {
+        bool f;
+        cin >> f;
+        //插入 
+        if (f == 1) {
+            int idx, n;
+            cin >> idx >> n;
+            insertFunc(&head,idx,n);
+        }
+        //删除
         if (f == 0) {
-            now = cur->val;
-            f = 1;
-            continue;
-        }
-        if (cur->val < now) {
-            now = cur->val;
-        }
-        else {
-            cout << "NO";
-            return 0;
+            int idx;
+            cin >> idx;
+            deletleFunc(&head,idx);
         }
     }
-    cout << "YES";
+    for (node* cur = head; cur != NULL; cur = cur->next) {
+        cout << cur->index;
+        if (cur->next != NULL) {
+            cout << " ";
+        }
+    }
     return 0;
 }
